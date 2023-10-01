b0c7a1723f3f4f2de727fb814ed9450779c6386d
André Koga <work@koga.one>
2023-10-01 14:52:29 -0300

    website test

---
+++ components/NavProject.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NavProject() {
+  return (
+    <Link href="">
+      <div className="aspect-video rounded-lg border border-black transition-colors hover:bg-black hover:text-white">
+        project #
+      </div>
+    </Link>
+  );
+}
