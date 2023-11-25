fcd581ac6787f2ff4a39d4588acba11a2e98bffd
S Taubman <akoga3@gatech.edu>
2023-11-25 15:45:13 -0500

    no dynamic routing

--- app/api/[name]/[id]/route.js
+++ app/api/[name]/[id]/route.js
@@ -1,28 +1,28 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
+// import sqlite3 from "sqlite3";
+// import { open } from "sqlite";
 
-let db = null;
+// let db = null;
 
-export async function GET(req, context) {
-  const id = context.params.id;
-  const name = context.params.name;
+// export async function GET(req, context) {
+//   const id = context.params.id;
+//   const name = context.params.name;
 
-  if (!db) {
-    db = await open({
-      filename: "db/db.sqlite3",
-      driver: sqlite3.Database,
-    });
-  }
+//   if (!db) {
+//     db = await open({
+//       filename: "db/db.sqlite3",
+//       driver: sqlite3.Database,
+//     });
+//   }
 
-  const item = await db.get(`SELECT * FROM ${name} WHERE id = ?`, id);
+//   const item = await db.get(`SELECT * FROM ${name} WHERE id = ?`, id);
 
-  return new Response(JSON.stringify(item), {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    },
-    status: 200,
-  });
-}
+//   return new Response(JSON.stringify(item), {
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Access-Control-Allow-Origin": "*",
+//       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+//       "Access-Control-Allow-Headers": "Content-Type, Authorization",
+//     },
+//     status: 200,
+//   });
+// }
