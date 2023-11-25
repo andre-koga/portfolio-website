bf8099b685e663412a84d55633d27e74daafc53a
S Taubman <akoga3@gatech.edu>
2023-11-25 15:12:27 -0500

    more cors

--- app/api/[name]/[id]/route.js
+++ app/api/[name]/[id]/route.js
@@ -20,6 +20,8 @@ export async function GET(req, context) {
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
     status: 200,
   });
